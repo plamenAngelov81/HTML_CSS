09b7cc26d7efa4db7598263bf7d634ab93772035
pachobg <pangelow9@gmail.com>
2023-02-07 22:57:00 +0200

    exam tasks one

--- HTML_CSS/examp_tasks/urls.py
+++ HTML_CSS/examp_tasks/urls.py
@@ -1,12 +1,26 @@
 from django.urls import path, include
 
-from HTML_CSS.examp_tasks.views import JunExamHomeView, HomeTaskView
+from HTML_CSS.examp_tasks.views import TaskOneHomeView, RegistrationFormView, MaineHomeView, SignUpFormView, KiwiView, \
+    FinancialServicesView, EnterpriseView, AutoView, MediumView, SimpleArticleView, RestaurantView, EarthView, \
+    AdvantagesView, LoginTaskView
 
 urlpatterns = [
     path('exams/', include([
-        path('html-css-exam-26-jun-2022/', include([
-            path('', JunExamHomeView.as_view(), name='26-jun-2022'),
-            path('26-2022-task-1', HomeTaskView.as_view(), name='26-jun-2022-task-1'),
+        path('task-1/', include([
+            path('', TaskOneHomeView.as_view(), name='task1 home'),
+            path('registration-form/', RegistrationFormView.as_view(), name='registration form'),
+            path('maine-home/', MaineHomeView.as_view(), name='maine home'),
+            path('sign-up/', SignUpFormView.as_view(), name='sign up'),
+            path('kiwi/', KiwiView.as_view(), name='kiwi'),
+            path('financial-services', FinancialServicesView.as_view(), name='financial services'),
+            path('enterprise/', EnterpriseView.as_view(), name='enterprise'),
+            path('auto/', AutoView.as_view(), name='auto'),
+            path('medium/', MediumView.as_view(), name='medium'),
+            path('simple article/', SimpleArticleView.as_view(), name='simple article'),
+            path('restaurant/', RestaurantView.as_view(), name='restaurant'),
+            path('earth/', EarthView.as_view(), name='earth'),
+            path('advantages/', AdvantagesView.as_view(), name='advantages'),
+            path('login/', LoginTaskView.as_view(), name='login'),
         ]))
     ]))
 ]
--- HTML_CSS/examp_tasks/views.py
+++ HTML_CSS/examp_tasks/views.py
@@ -1,10 +1,57 @@
 from django.views import generic
 
 
-class JunExamHomeView(generic.TemplateView):
-    template_name = 'exams/HTML & CSS Exam - 26 Jun 2022/26_jun_2022_home_page.html'
+class TaskOneHomeView(generic.TemplateView):
+    template_name = 'exams/task_1/home_task_1.html'
 
 
-class HomeTaskView(generic.TemplateView):
-    template_name = 'exams/HTML & CSS Exam - 26 Jun 2022/home.html'
+class RegistrationFormView(generic.TemplateView):
+    template_name = 'exams/task_1/registration_form.html'
 
+
+class MaineHomeView(generic.TemplateView):
+    template_name = 'exams/task_1/maine_home.html'
+
+
+class SignUpFormView(generic.TemplateView):
+    template_name = 'exams/task_1/sign_up_form.html'
+
+
+class KiwiView(generic.TemplateView):
+    template_name = 'exams/task_1/kiwi.html'
+
+
+class FinancialServicesView(generic.TemplateView):
+    template_name = 'exams/task_1/financial_services.html'
+
+
+class EnterpriseView(generic.TemplateView):
+    template_name = 'exams/task_1/enterprise.html'
+
+
+class AutoView(generic.TemplateView):
+    template_name = 'exams/task_1/auto.html'
+
+
+class MediumView(generic.TemplateView):
+    template_name = 'exams/task_1/medium.html'
+
+
+class SimpleArticleView(generic.TemplateView):
+    template_name = 'exams/task_1/simple_article.html'
+
+
+class RestaurantView(generic.TemplateView):
+    template_name = 'exams/task_1/restaurant.html'
+
+
+class EarthView(generic.TemplateView):
+    template_name = 'exams/task_1/earth.html'
+
+
+class AdvantagesView(generic.TemplateView):
+    template_name = 'exams/task_1/advantages.html'
+
+
+class LoginTaskView(generic.TemplateView):
+    template_name = 'exams/task_1/login.html'
